329ab3e75b437c0d5fa8e18b208e2009ff74eee9
Nafakhrian <Nafakhrian@users.noreply.github.com>
2020-02-06 10:29:04 +0700

    Layouting

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,32 +1,30 @@
-<!doctype html>
-<html lang="en">
+@extends('template')
 
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
-        crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
-        crossorigin="anonymous">
-
-    <title>UTS</title>
-</head>
-
-<body onload="readTask()">
-
-    <nav class="navbar navbar-light bg-primary mb-4">
-        <span class="navbar-brand mb-0 h1">Data Karyawan</span>
-    </nav>
-
-    <div class="container">
+@section('content')
+    <!-- main content -->
+    <div class="container-fluid ">
         <div class="row">
-            <div class="col-md-10" id="firstSection" style="margin: 0 auto;">
-                <form class="border p-4 mb-4" id="form" autocomplete="off">
 
-                    <!-- @if (!empty($filltable)) -->
+            <div class="col-xl-12 " >
+              <div class="card shadow mb-4">
+                <!-- Card Header - Dropdown -->
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                  <h6 class="m-0 font-weight-bold text-primary">Tabel karyawan</h6>
+                  <div class="col-md-1">
+                      <fieldset>
+                        <div id="box2">
+                            <button class="btn btn-primary" name="btn-insert"> Insert</button>
+                        </div>
+                      </fieldset>
+                  </div>
+                </div>
+                <!-- Card Body -->
+                <div class="card-body">
+                    
+                    <form class="p-4 mb-4" id="form" autocomplete="off">
+                    
+                    @if (!empty($filltable))
                     <table align="center" border="1">
                         <thead>
                             <tr style="text-align: center;">
@@ -35,6 +33,7 @@
                                 <th width="200px">Nama</th>
                                 <th width="200px">Alamat</th>
                                 <th width="250px">Email</th>
+                                <th>Action</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -45,34 +44,30 @@
                                 <td>{{ $table->nama }}</td>
                                 <td>{{ $table->alamat }}</td>
                                 <td>{{ $table->email }}</td>
+                                <td><button class="btn btn-info" name="btn-update" style="margin: 5px; margin-right: 0px;"> Update</button>
+                                    <button class="btn btn-danger" name="btn-delete" style="margin: 5px; margin-left: 0px"> Delete</button></td>
                             </tr>
                             @endforeach
                         </tbody>
                     </table>
 
-                    <!-- @else -->
+                    @else
                     <h2>Tidak ada data karyawan</h2>
 
-                    <!-- @endif -->
+                    @endif
 
                 </form>
+                    
+                </div>
+              </div>
             </div>
+
+           
             <div class="col-md-6" id="cardSection">
 
             </div>
         </div>
     </div>
+    <!-- end main content -->
+@stop
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
-        crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
-        crossorigin="anonymous"></script>
-   <script src="https://www.gstatic.com/firebasejs/5.9.2/firebase.js"></script>
-    <script src="index.js"></script>
-</body>
-
-</html>
